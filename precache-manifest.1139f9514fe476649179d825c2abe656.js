cdfeae617e211d2092c3ad2599743caf092eec8e
lavish jain <lavishjain36@gmail.com>
2020-08-09 17:12:18 +0530

    Updates

---
+++ precache-manifest.1139f9514fe476649179d825c2abe656.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f8ed31fe381ba01f56799ec402468f",
+    "url": "/lavishjain36/todoapp/index.html"
+  },
+  {
+    "revision": "8897d02d40f0b4bd1bb1",
+    "url": "/lavishjain36/todoapp/static/css/main.694312b5.chunk.css"
+  },
+  {
+    "revision": "5db7f2d4b03ed35435c7",
+    "url": "/lavishjain36/todoapp/static/js/2.03b3a3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lavishjain36/todoapp/static/js/2.03b3a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8897d02d40f0b4bd1bb1",
+    "url": "/lavishjain36/todoapp/static/js/main.90ed330b.chunk.js"
+  },
+  {
+    "revision": "0f7bd5b8cb0b8e390154",
+    "url": "/lavishjain36/todoapp/static/js/runtime-main.9a095c78.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lavishjain36/todoapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
